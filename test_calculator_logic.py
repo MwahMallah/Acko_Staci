99b211c85ea9c047b9aae2ac07bc1175da7a120d
aanastsaa <xsamoi00@vutbr.cz>
2023-03-31 19:45:33 +0200

    tests for program

--- test_calculator_logic.py
+++ test_calculator_logic.py
@@ -1,30 +1,52 @@
-def test_can_add_two_numbers():
-    pass
+# Test for IVS projekt 2, calculator.py
 
-def test_can_sub_two_numbers():
-    pass
-
-# Function for calculator in projekt IVS
-
+import pytest
+from calcularor_logic import Calc
 import math
 
-def sum(a, b):
-    # Function for sum, where a and b are numbers and result is sum of a and b
+def test_sum(self):
+   self.assertEqual(calc.sum(59, 9), 68)
+   self.assertEqual(calc.sum(-7, 7), 0)
+   self.assertEqual(calc.sum(-45, -45), -90)
+   self.assertEqual(calc.sum(0, 0), 0)
 
-def substaction(a, b):
-    # Function for sub, where a and b are numbers and result is substaction of a and b
+def test_substaction(self):
+   self.assertEqual(calc.substaction(59, 9), 50)
+   self.assertEqual(calc.substaction(-7, 7), -14)
+   self.assertEqual(calc.substaction(-45, -45), 0)
+   self.assertEqual(calc.substaction(0, 0), 0)
 
-def multiplication(a, b):
-    # Function for multiplication, where a and b are numbers and result is multiplication of a and b
+def test_multiplication(self):
+   self.assertEqual(calc.multiplication(59, 9), 531)
+   self.assertEqual(calc.multiplication(-7, 7), -49)
+   self.assertEqual(calc.multiplication(-45, 0), 0)
+   self.assertEqual(calc.multiplication(543, 1), 543)
 
-def division(a, b):
-    # Function for division, where a and b are numbers and result is division of a and b
+def test_division(self):
+   self.assertEqual(calc.division(59, 9), 6.555555555555555)
+   self.assertEqual(calc.division(-7, 7), -1)
+   self.assertEqual(calc.division(14, 7), 2)
+   self.assertEqual(calc.division(-45, 0), "Error")
+   self.assertEqual(calc.division(0, 78), 0)
 
-def sqrt(a):
-    # Function for sqrt, where a is number and result is sqrt of a
+def test_sqrt(self):
+   self.assertEqual(calc.sqrt(84), 9.16515138991168)
+   self.assertEqual(calc.sqrt(0), 0)
+   self.assertEqual(calc.sqrt(1), 1)
+   self.assertEqual(calc.sqrt(49), 7) 
+   self.assertEqual(calc.sqrt(-7), "Error")
 
-def square(a, b):
-    # a is number and b is power, where result is a^b
+def test_square(self):
+   self.assertEqual(calc.square(7, 2), 49)
+   self.assertEqual(calc.square(0, 0), 0)
+   self.assertEqual(calc.square(1, 1), 1)
+   self.assertEqual(calc.square(2, 8), 256)
+   self.assertEqual(calc.square(-7, 5), -16807)
+   self.assertEqual(calc.square(15, -2), 0.00390625)
 
-def factorial(a):
-    # Function for factorial, where a is number and result is factorial of a
+def test_factorial(self):
+   self.assertEqual(calc.factorial(33), 8683317618811886495518194401280000000)
+   self.assertEqual(calc.factorial(0), 1)
+   self.assertEqual(calc.factorial(1), 1)
+   self.assertEqual(calc.factorial(4), 24)
+   self.assertEqual(calc.factorial(-7), "Error")
