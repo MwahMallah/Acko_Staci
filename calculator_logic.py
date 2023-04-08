482670a34293601d03341253e272e1b3e9dd655d
tomaar <tomaar19@gmail.com>
2023-04-08 19:32:54 +0200

    added adding

--- calculator_logic.py
+++ calculator_logic.py
@@ -10,11 +10,11 @@ def expression_to_list(expr):
             prev_numeric += list_char
         else:
             if prev_numeric != "":
-                list_split.append(prev_numeric)
+                list_split.append(int(prev_numeric))
             prev_numeric = ""
             list_split.append(list_char)
     if prev_numeric != "":
-        list_split.append(prev_numeric)
+        list_split.append(int(prev_numeric))
     return list_split
 
 
@@ -23,10 +23,25 @@ def remove_spaces_from_list(expr):
     return list_despaced
 
 
+# executes expression
+def execute(arg1=None, arg2=None, operation=None):
+    match operation:
+        case "+":
+            return arg1 + arg2
+    #     case "-":
+    #         return self.first_arg - self.second_arg
+    #     case "/":
+    #         return self.first_arg / self.second_arg
+    #     case "*":
+    #         return self.first_arg * self.second_arg
+    #     case "!":
+    #         return math.factorial(self.first_arg)
+    #     case "^":
+    #         return math.pow(self.first_arg, self.second_arg)
+
+
 class Calc():
     def __init__(self):
-        self.first_arg = None
-        self.second_arg = None
         self.expression = None
 
     # takes input from the calculator input line
@@ -39,56 +54,29 @@ class Calc():
     def parse(self):
         self.expression = expression_to_list(self.expression)
         self.expression = remove_spaces_from_list(self.expression)
-        brack_contents = list()
-        brack_ctr = 0
+        # brack_contents = ""
+        # brack_ctr = 0
         # while "(" in self.expression:
-        for list_item in self.expression:
-            if list_item == "(":
-                brack_ctr += 1
-            elif list_item == ")":
-                brack_ctr -= 1
+        #     for list_item in self.expression:
+        #         if list_item == "(":
+        #             brack_ctr += 1
+        #         elif list_item == ")":
+        #             brack_ctr -= 1
+        #         if brack_ctr < 0:
+        #             pass  # error
+        #         if brack_ctr == 0 and brack_contents is not None:
+        #             brack_contents = brack_contents[1:]
+        #             #print(brack_contents)
+        #             pass  # přidat počítání v závorce
+        #         elif brack_ctr > 0:
+        #             brack_contents += list_item
 
-            if brack_ctr < 0:
-                pass  # error
-            if brack_ctr == 0 and brack_contents is not None:
-                brack_contents.pop(0)
-                pass  # přidat počítání v závorce
-            elif brack_ctr > 0:
-                brack_contents.append(list_item)
+        # while "+" in self.expression:
+        op_index = self.expression.index("+")
+        self.expression[op_index] = execute(self.expression[op_index - 1],
+                                            self.expression[op_index + 1],
+                                            "+")
+        self.expression.pop(op_index - 1)
+        self.expression.pop(op_index)
+
         return self.expression
-
-    # executes expression
-    def exec(self):
-        list_of_args = self.expression.split()
-        try:
-            self.first_arg = int(list_of_args[0])
-        except IndexError:
-            pass
-
-        try:
-            self.second_arg = int(list_of_args[2])
-        except IndexError:
-            pass
-
-        operation = list_of_args[1]
-
-        match operation:
-            case "+":
-                return self.first_arg + self.second_arg
-            case "-":
-                return self.first_arg - self.second_arg
-            case "/":
-                return self.first_arg / self.second_arg
-            case "*":
-                return self.first_arg * self.second_arg
-            case "!":
-                return math.factorial(self.first_arg)
-            case "^":
-                return math.pow(self.first_arg, self.second_arg)
-
-    def sqrt(self, a):
-        # Function for sqrt, where a is number and result is sqrt of a
-        return math.sqrt(a)
-
-    def logarithm(self, number, base):
-        return math.log(number, base)
