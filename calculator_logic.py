d18e729914a6b8b5d46b6df7878e787cb4cd1371
tomaar <tomaar19@gmail.com>
2023-04-10 18:57:00 +0200

    powers, factorial handling

--- calculator_logic.py
+++ calculator_logic.py
@@ -3,21 +3,39 @@ import math
 
 def expression_to_list(expr):
     list(expr)
+    list_split_temp = list()
     list_split = list()
+    prev_alpha = ""
     prev_numeric = ""
+
     for list_char in expr:
-        if list_char.isnumeric() or list_char == ".":
+        if list_char.isalpha():
+            prev_alpha += list_char
+        else:
+            if prev_alpha != "":
+                list_split_temp.append(prev_alpha)
+            prev_alpha = ""
+            list_split_temp.append(list_char)
+    if prev_alpha != "":
+        list_split_temp.append(prev_alpha)
+
+    for list_char in list_split_temp:
+        if list_char.isnumeric() or list_char == "." or (list_char == "-" and prev_numeric == ""):
             prev_numeric += list_char
         else:
             if prev_numeric != "":
-                if "." in prev_numeric:
+                if prev_numeric == "-":
+                    list_split.append(prev_numeric)
+                elif "." in prev_numeric:
                     list_split.append(float(prev_numeric))
                 else:
                     list_split.append(int(prev_numeric))
             prev_numeric = ""
             list_split.append(list_char)
     if prev_numeric != "":
-        if "." in prev_numeric:
+        if prev_numeric == "-":
+            list_split.append(prev_numeric)
+        elif "." in prev_numeric:
             list_split.append(float(prev_numeric))
         else:
             list_split.append(int(prev_numeric))
@@ -30,7 +48,7 @@ def remove_spaces_from_list(expr):
 
 
 # executes expression
-def execute(arg1=None, arg2=None, operation=None):
+def execute(operation, arg1=None, arg2=None):
     match operation:
         case "+":
             return arg1 + arg2
@@ -39,7 +57,21 @@ def execute(arg1=None, arg2=None, operation=None):
         case "*":
             return arg1 * arg2
         case "/":
-            return arg1 / arg2
+            if arg2 == 0:
+                return "Error"
+            else:
+                return arg1 / arg2
+        case "^":
+            res = pow(arg1, arg2)
+            if isinstance(res, complex):
+                return "Error"
+            else:
+                return res
+        case "!":
+            if (isinstance(arg1, int) or arg1.is_integer()) and arg1 >= 0:
+                return math.factorial(arg1)
+            else:
+                return "Error"
 
 
 class Calc():
@@ -56,22 +88,57 @@ class Calc():
     def parse(self):
         self.expression = expression_to_list(self.expression)
         self.expression = remove_spaces_from_list(self.expression)
-        # brack_contents = ""
-        # brack_ctr = 0
-        # while "(" in self.expression:
-        #     for list_item in self.expression:
-        #         if list_item == "(":
-        #             brack_ctr += 1
-        #         elif list_item == ")":
-        #             brack_ctr -= 1
-        #         if brack_ctr < 0:
-        #             pass  # error
-        #         if brack_ctr == 0 and brack_contents is not None:
-        #             brack_contents = brack_contents[1:]
-        #             #print(brack_contents)
-        #             pass  # přidat počítání v závorce
-        #         elif brack_ctr > 0:
-        #             brack_contents += list_item
+        brack_contents = ""
+        brack_ctr = 0
+        while "(" in self.expression:
+            pos_ctr = 0
+            for list_item in self.expression[::]:
+                if list_item == "(":
+                    brack_ctr += 1
+                elif list_item == ")":
+                    brack_ctr -= 1
+                if brack_ctr < 0:
+                    pass  # error
+                if brack_ctr == 0 and brack_contents is not None:
+                    brack_contents = brack_contents[1:]
+                    brack_sub = Calc()
+                    brack_sub.take_expression(brack_contents)
+                elif brack_ctr > 0:
+                    brack_contents += str(list_item)
+                    self.expression.pop(pos_ctr)
+                pos_ctr += 1
+
+        while "!" in self.expression:
+            op_index = self.expression.index("!")
+
+            self.expression[op_index] = execute(self.expression[op_index],
+                                                self.expression[op_index - 1])
+            if self.expression[op_index] == "Error":
+                return TypeError
+            self.expression.pop(op_index - 1)
+
+        while "sqrt" in self.expression:
+            op_index = self.expression.index("sqrt")
+            self.expression[op_index], self.expression[op_index + 1] \
+                = self.expression[op_index + 1], 1 / 2
+            self.expression.insert(op_index + 1, "^")
+
+        while "cbrt" in self.expression:
+            op_index = self.expression.index("cbrt")
+            self.expression[op_index], self.expression[op_index + 1] \
+                = self.expression[op_index + 1], 1 / 3
+            self.expression.insert(op_index + 1, "^")
+
+        while "^" in self.expression:
+            op_index = self.expression.index("^")
+
+            self.expression[op_index] = execute(self.expression[op_index],
+                                                self.expression[op_index - 1],
+                                                self.expression[op_index + 1])
+            if self.expression[op_index] == "Error":
+                return "Error"
+            self.expression.pop(op_index - 1)
+            self.expression.pop(op_index)
 
         while "*" in self.expression or "/" in self.expression:
             if "*" in self.expression:
@@ -85,9 +152,9 @@ class Calc():
             else:
                 op_index = self.expression.index("/")
 
-            self.expression[op_index] = execute(self.expression[op_index - 1],
-                                                self.expression[op_index + 1],
-                                                self.expression[op_index])
+            self.expression[op_index] = execute(self.expression[op_index],
+                                                self.expression[op_index - 1],
+                                                self.expression[op_index + 1])
             self.expression.pop(op_index - 1)
             self.expression.pop(op_index)
 
@@ -103,10 +170,15 @@ class Calc():
             else:
                 op_index = self.expression.index("-")
 
-            self.expression[op_index] = execute(self.expression[op_index - 1],
-                                                self.expression[op_index + 1],
-                                                self.expression[op_index])
+            self.expression[op_index] = execute(self.expression[op_index],
+                                                self.expression[op_index - 1],
+                                                self.expression[op_index + 1])
+            if self.expression[op_index] == "Error":
+                return "Error"
             self.expression.pop(op_index - 1)
             self.expression.pop(op_index)
 
-        return self.expression[0]
+        if isinstance(self.expression[0], float) and self.expression[0].is_integer():
+            return int(self.expression[0])
+        else:
+            return self.expression[0]
