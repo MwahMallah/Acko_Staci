1b2bb43e828b49b037a8b85273108d47e1daadc6
John Doe <maksim@gmail.com>
2023-04-20 13:58:32 +0200

    finished first version of gui.py

---
+++ gui.py
@@ -0,0 +1,73 @@
+from PyQt6.QtCore import Qt 
+
+import sys
+
+from PyQt6.QtWidgets import QApplication, QPushButton, QLineEdit, QMainWindow, QVBoxLayout, QGridLayout, QWidget
+
+
+WINDOW_WIDTH = 330
+WINDOW_HEIGHT = 300
+
+DISPLAY_HEIGHT = 35
+
+BUTTON_SIZE = 40
+
+
+class Display(QLineEdit):
+    def __init__(self):
+        super().__init__()
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.setFixedHeight(DISPLAY_HEIGHT)
+
+class Buttons(QWidget):
+    def __init__(self):
+        super().__init__()        
+        layout = QGridLayout()
+        keyboard = [["7", "8", "9", "/", "C"],
+        ["4", "5", "6", "*", "("],
+        ["1", "2", "3", "-", ")"],
+        ["0", "00", ".", "+", "="]]
+
+        keymap = {}
+
+        for row, key in enumerate(keyboard):
+            for column, btn in enumerate(key):
+                keymap[btn] = QPushButton(btn)
+                keymap[btn].setFixedSize(int(BUTTON_SIZE*1.5), BUTTON_SIZE)
+                layout.addWidget(keymap[btn], row, column)
+
+        self.setLayout(layout)
+
+
+class CalculatorUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+        centralWidget = QWidget(self)
+
+        layout =  QVBoxLayout()
+        self.display = Display()
+        buttons = Buttons()
+        layout.addWidget(self.display)
+        layout.addWidget(buttons)
+
+        centralWidget.setLayout(layout)
+
+        self.setCentralWidget(centralWidget)
+
+    def setDisplayLine(self, text):
+        self.display.setText(text)
+
+    def deleteLine(self):
+        self.setDisplayLine("")
+
+    def getDisplayLine(self):
+        return self.display.text()
+
+
+app = QApplication([])
+
+window = CalculatorUI()
+window.show()
+
+sys.exit(app.exec())
--- help.py
+++
@@ -1,7 +0,0 @@
-from calculator_logic import Calc
-
-expr = "3 !"
-
-calculator = Calc(expr)
-
-print(calculator.exec())
